3e1ae1fd1c3eedc41e2b467e2a09d81242f62e29
Nacho G. <igonza41@xtec.cat>
2025-04-25 19:42:09 +0200

    Implementation for DateTimeUtils

--- HobbyManiaManager/Utils/DateTimeUtils.cs
+++ HobbyManiaManager/Utils/DateTimeUtils.cs
@@ -8,5 +8,24 @@ namespace HobbyManiaManager.Utils
 {
     class DateTimeUtils
     {
+        public static int GetDifferenceInDays(DateTime startDate, DateTime endDate)
+        {
+            return (int)(endDate - startDate).Duration().TotalDays;
+        }
+
+        public static double GetDifferenceInSeconds(DateTime startDate, DateTime endDate)
+        {
+            return (endDate - startDate).Duration().TotalSeconds;
+        }
+
+        public static double GetDifferenceInMinutes(DateTime startDate, DateTime endDate)
+        {
+            return (endDate - startDate).Duration().TotalMinutes;
+        }
+
+        public static double GetDifferenceInHours(DateTime startDate, DateTime endDate)
+        {
+            return (endDate - startDate).Duration().TotalHours;
+        }
     }
 }
